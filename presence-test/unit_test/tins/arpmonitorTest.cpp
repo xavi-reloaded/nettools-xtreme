ba5c58b776aa1c34b963beeb6042efbd2afafbad
xavi <xavi.hidalgo.fernandez@gmail.com>
2018-06-11 21:11:52 +0200

    arp monitor

---
+++ presence-test/unit_test/tins/arpmonitorTest.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "src/tins/arpmonitor.cpp"
+
+
+
+
+TEST(arpmonitor, init)
+{
+    arp_monitor monitor;
+    // Sniffer configuration
+    SnifferConfiguration config;
+    config.set_promisc_mode(true);
+    config.set_filter("arp");
+
+    Sniffer sniffer("wlp2s0", config);
+    // Only capture arp packets
+    monitor.run(sniffer);
+
+    EXPECT_EQ(1, true);
+}
