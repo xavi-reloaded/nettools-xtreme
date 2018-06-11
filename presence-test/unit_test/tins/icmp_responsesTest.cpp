bf1d95b1c2dfe15f303ffcb2200dc5c25dce8ad7
xavi <xavi.hidalgo.fernandez@gmail.com>
2018-06-11 23:45:06 +0200

    arp monitor

---
+++ presence-test/unit_test/tins/icmp_responsesTest.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "src/tins/icmp_responses.cpp"
+
+
+
+
+TEST(icmp_responses, init)
+{
+    const int type = 3;
+    const int code = 0;
+    NetworkInterface iface;
+    iface = NetworkInterface::default_interface();
+    string filter = "tcp";
+    try {
+        ICMPResponder responder(iface.name(), type, code);
+        responder.run(filter);
+    }
+    catch (exception& ex) {
+        cout << "Error: " << ex.what() << endl;
+    }
+
+
+}
